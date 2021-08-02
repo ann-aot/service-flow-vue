46b3081ac878e2ae32b7e239ea0c2b94e5f53069
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-08-02 20:32:11 +0530

    update keycloak eg based on official doc, which reloads always

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,60 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import Keycloak, { KeycloakInitOptions, KeycloakInstance, KeycloakLoginOptions, KeycloakTokenParsed } from 'keycloak-js'
+import { BootstrapVue } from "bootstrap-vue";
 import Vue from "vue";
 import App from "./App.vue";
+// import authentication from "@/plugins/authentication";
+// import plugin from "@/plugins/plugin";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+
+const initOptions = {
+  url: 'https://iam.aot-technologies.com/auth', realm: 'formsflow-ai-willow', clientId: 'forms-flow-web', onLoad: 'login-required'
+}
+
+const keycloak = Keycloak(initOptions);
+
+// Vue.$keycloak.init({
+//   checkLoginIframe: false})
+//   .then(() => {
+keycloak.init({onLoad: 'login-required' }).then((auth) => {
+  console.log("authentication status", auth)
+  if (!auth) {
+    window.location.reload();
+  } else {
+    console.log('Authenticated!');
+    new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
+  // })
+  }
+
+  console.log("token", keycloak.token)
+
+   setInterval(() => {
+    keycloak.updateToken(70).then((refreshed) => {
+      if (refreshed) {
+        console.info('Token refreshed' + refreshed);
+      } else {
+        // console.warn('Token not refreshed, valid for '
+        //   + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+      }
+    }).catch(() => {
+      console.error('Failed to refresh token');
+    });
+  }, 30000)
+
+}).catch(() => {
+  console.error("Authenticated Failed");
+
+
+});
+
+
